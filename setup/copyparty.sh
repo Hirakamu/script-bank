3ec8b2942a06ea4f014b5066984479eb47a0688c
Hirakamu <achmadirsyadhamizan@gmail.com>
2025-09-28 12:46:41 +0700

    update

--- setup/copyparty.sh
+++ setup/copyparty.sh
@@ -6,24 +6,34 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 echo "Setting up copyparty..."
-sleep 2
+sleep 1
 
 # Create copyparty user
-useradd -r -s /sbin/nologin -m -d /var/lib/copyparty copyparty
+id copyparty &>/dev/null || useradd -r -s /sbin/nologin -m -d /var/lib/copyparty copyparty
 
 # Install core app
-wget https://github.com/9001/copyparty/releases/latest/download/copyparty-sfx.py -O /usr/local/bin/copyparty-sfx.py
+wget -q --show-progress https://github.com/9001/copyparty/releases/latest/download/copyparty-sfx.py -O /usr/local/bin/copyparty-sfx.py
 chmod +x /usr/local/bin/copyparty-sfx.py
 
-# Download default config
-wget https://raw.githubusercontent.com/hirakamu/script-bank/main/files/copyparty/copyparty.conf -O /etc/copyparty.conf
+# Ask for config
+read -rp "Do you want to install the default config? [Y/n] " ans
+if [[ "$ans" =~ ^[Yy]$|^$ ]]; then
+  wget -q --show-progress https://raw.githubusercontent.com/hirakamu/script-bank/main/files/copyparty/copyparty.conf -O /etc/copyparty.conf
+  echo "Config installed at /etc/copyparty.conf"
+fi
 
-# Setup systemd service
-wget https://raw.githubusercontent.com/hirakamu/script-bank/main/files/copyparty/copyparty.service -O /etc/systemd/system/copyparty.service
-systemctl daemon-reload
-systemctl enable copyparty.service
-systemctl start copyparty.service
-systemctl restart copyparty.service
+# Ask for service
+read -rp "Do you want to install the systemd service file? [Y/n] " ans
+if [[ "$ans" =~ ^[Yy]$|^$ ]]; then
+  wget -q --show-progress https://raw.githubusercontent.com/hirakamu/script-bank/main/files/copyparty/copyparty.service -O /etc/systemd/system/copyparty.service
+  systemctl daemon-reload
+  systemctl enable copyparty.service
+  systemctl restart copyparty.service
+  echo "Service enabled and started."
+fi
 
-echo "Currently youre using the default config. Please do 'sudo nano /etc/copyparty.conf' to edit and configure."
-echo "Copyparty setup complete. The service is now running."
+echo "Copyparty setup complete."
+sleep 1
+echo "You can edit the config file at /etc/copyparty.conf"
+echo "Then start/restart the service with: systemctl start|restart copyparty.service"
+echo "Access the web interface at http://<your-ip>:8000"
