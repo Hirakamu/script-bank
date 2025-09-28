0e488744fab103708f1efa774bce2209f2a1d2ca
Hirakamu <achmadirsyadhamizan@gmail.com>
2025-09-28 12:31:59 +0700

    copyparty

--- setup/copyparty.sh
+++ setup/copyparty.sh
@@ -1,10 +1,28 @@
-!#/bin/bash
+#!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root." >&2
+  exit 1
+fi
+
+echo "Setting up copyparty..."
 
 # Install core app
 wget https://github.com/9001/copyparty/releases/latest/download/copyparty-sfx.py -O /usr/local/bin/copyparty-sfx.py
 chmod +x /usr/local/bin/copyparty-sfx.py
-ln -s /usr/local/bin/copyparty-sfx.py /usr/local/bin/copyparty
 
+# Create copyparty user
 useradd -r -s /sbin/nologin -m -d /var/lib/copyparty copyparty
 
+# Download default config
+wget https://raw.githubusercontent.com/hirakamu/script-bank/main/files/copyparty/copyparty.conf -O /var/lib/copyparty/copyparty.conf
+
+echo "Currently youre using the default config. Please edit /var/lib/copyparty/copyparty.conf as needed."
+
+# Setup systemd service
+wget https://raw.githubusercontent.com/hirakamu/script-bank/main/setup/copyparty.service -O /etc/systemd/system/copyparty.service
+systemctl daemon-reload
+systemctl enable copyparty.service
+systemctl start copyparty.service
+
+echo "Copyparty setup complete. The service is now running."
